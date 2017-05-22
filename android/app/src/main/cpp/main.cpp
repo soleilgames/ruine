dccacb583a19c30251501d666df0fddd875e4694
Florian GOLESTIN <florian.golestin@gmail.com>
2017-05-22 16:33:06 +0200

    Initial commit :)

---
+++ android/app/src/main/cpp/main.cpp
@@ -0,0 +1,17 @@
+
+
+#include <memory>
+
+#include <android_native_app_glue.h>
+
+/**
+ * This is the main entry point of a native application that is using
+ * android_native_app_glue.  It runs in its own thread, with its own
+ * event loop for receiving input events and doing other things.
+ */
+void
+android_main(struct android_app* state)
+{
+  // Make sure glue isn't stripped.
+  app_dummy();
+}
